dd9155e4878b5d8feb974738df5d138d8c452d29
AbdullahJUTT <abdullahahmad319.319@gmail.com>
2023-07-27 03:33:02 +0500

    Add existing project files to Git

---
+++ CRUD_Operations/Models/Transaction.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_Operations.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        [Required]
+        [MaxLength(12, ErrorMessage ="Max characters 12 only")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        [Required]
+        [MaxLength(11, ErrorMessage = "Max characters 11 only")]
+        public string SWIFTCode { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MMM-dd-yy}")]
+        public DateTime Date { get; set; }
+
+
+    }
+}
